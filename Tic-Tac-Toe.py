bf451619148125a8784e537a073efb7904b82906
Bookboy2000 <kbwbookboy@gmail.com>
2022-01-11 20:14:08 -0700

    Tic-Tac-Toe game

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -6,22 +6,35 @@ def main():
     x_games = 0
     num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
     print("This is a first to 3 wins match.")
-    print()
 
     while game == True:
-        print(f"Scores: o [{o_games} : {x_games}] x")
+        print(f"\nScores: o [{o_games} : {x_games}] x")
         print()
         print(*num_list, sep= "/")
         if o == True:
             user = str(input("\no's turn. Choose 1-9: "))
             assign_o(user, num_list)
-            o_games = find_outcome_o(num_list, o_games)
+            num_list, o_games = find_outcome_o(num_list, o_games)
+            num_list = test_draw(num_list)
+            if o_games == 3:
+                print("\n   * * * * * * * * * * * * * *   ")
+                print("~~~Player x has won the match!~~~")
+                print("   * * * * * * * * * * * * * *   ")
+                print("\n If you would like to play again kill this terminal and restart.")
+                game = False
             x = True
             o = False
         elif x == True:
             user = str(input("\nx's turn. Choose 1-9: "))
             assign_x(user, num_list)
-            x_games = find_outcome_x(num_list, x_games)
+            num_list, x_games = find_outcome_x(num_list, x_games)
+            num_list = test_draw(num_list)
+            if x_games == 3:
+                print("\n   * * * * * * * * * * * * * *   ")
+                print("~~~Player x has won the match!~~~")
+                print("   * * * * * * * * * * * * * *   ")
+                print("\n If you would like to play again kill this terminal and restart.")
+                game = False
             o = True
             x = False
 
@@ -54,142 +67,128 @@ def assign_x(user, num_list):
 
 
 def find_outcome_o(num_list, o_games):
-    # HERE IS THE ISSUE
     for i in range (len(num_list)):
-        if num_list[1] and num_list[2] and num_list[3] == "o" or "o\n":
+        if num_list[1] == "o" and num_list[2] == "o" and num_list[3] == "o\n":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
-        elif num_list[4] and num_list[5] and num_list[6] == "o" or "o\n":
+        elif num_list[4] == "o" and num_list[5] == "o" and num_list[6] == "o\n":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
-        elif num_list[7] and num_list[8] and num_list[9] == "o" or "o\n":
+        elif num_list[7] == "o" and num_list[8] == "o" and num_list[9] == "o":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
-        elif num_list[1] and num_list[4] and num_list[7] == "o" or "o\n":
+        elif num_list[1] == "o" and num_list[4] == "o" and num_list[7] == "o":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
-        elif num_list[2] and num_list[5] and num_list[8] == "o" or "o\n":
+        elif num_list[2] == "o" and num_list[5] == "o" and num_list[8] == "o":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
-        elif num_list[3] and num_list[6] and num_list[9] == "o" or "o\n":
+        elif num_list[3] == "o\n" and num_list[6] == "o\n" and num_list[9] == "o":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
-        elif num_list[1] and num_list[5] and num_list[9] == "o" or "o\n":
+        elif num_list[1] == "o" and num_list[5] == "o" and num_list[9] == "o":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
-        elif num_list[3] and num_list[5] and num_list[7] == "o" or "o\n":
+        elif num_list[3] == "o\n" and num_list[5] == "o" and num_list[7] == "o":
             print("\nPlayer o has won a game!")
             o_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return o_games
+            return num_list, o_games
 
         else:
-            pass
+            num_list = num_list
+            o_games += 0
+            return num_list, o_games
 
     
 def find_outcome_x(num_list, x_games):
     for i in range (len(num_list)):
-        if num_list[1] and num_list[2] and num_list[3] == "x" or "x\n":
+        if num_list[1] == "x" and num_list[2] == "x" and num_list[3] == "x\n":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
 
-        elif num_list[4] and num_list[5] and num_list[6] == "x" or "x\n":
+        elif num_list[4] == "x" and num_list[5] == "x" and num_list[6] == "x\n":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
 
-        elif num_list[7] and num_list[8] and num_list[9] == "x" or "x\n":
+        elif num_list[7] == "x" and num_list[8] == "x" and num_list[9] == "x":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
 
-        elif num_list[1] and num_list[4] and num_list[7] == "x" or "x\n":
+        elif num_list[1] == "x" and num_list[4] == "x" and num_list[7] == "x":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
 
-        elif num_list[2] and num_list[5] and num_list[8] == "x" or "x\n":
+        elif num_list[2] == "x" and num_list[5] == "x" and num_list[8] == "x":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
 
-        elif num_list[3] and num_list[6] and num_list[9] == "x" or "x\n":
+        elif num_list[3] == "x\n" and num_list[6] == "x\n" and num_list[9] == "x":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
 
-        elif num_list[1] and num_list[5] and num_list[9] == "x" or "x\n":
+        elif num_list[1] == "x" and num_list[5] == "x" and num_list[9] == "x":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
 
-        elif num_list[3] and num_list[5] and num_list[7] == "x" or "x\n":
+        elif num_list[3] == "x\n" and num_list[5] == "x" and num_list[7] == "x":
             print("\nPlayer x has won a game!")
             x_games += 1
             num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
-            return x_games
+            return num_list, x_games
+            
 
         else:
-            pass
-        
+            x_games += 0
+            num_list = num_list
+            return num_list, x_games
 
 
-
-
-
-    #if user == num1:
-        #num1 = "o"
-    #elif user == num2:
-        #num2 = "o"
-    #elif user == num3:
-        #num3 = "o"
-    #elif user == num4:
-        #num4 = "o"
-    #elif user == num5:
-        #num5 = "o"
-    #elif user == num6:
-        #num6 = "o"
-    #elif user == num7:
-        #num7 = "o"
-    #elif user == num8:
-        #num8 = "o"
-    #elif user == num9:
-        #num9 = "o"
-    #else:
-        #print("\nSorry, that box is unavailable.")
-
-
-
-
+def test_draw(num_list):
+    nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    is_there = any([num in num_list for num in nums])
+    if is_there == False:
+        num_list = ["", "1", "2", "3\n", "4", "5", "6\n", "7", "8", "9"]
+        print("\n\nDraw! Try again")
+        return num_list
+    else:
+        num_list = num_list
+        return num_list
 
 
 main()
